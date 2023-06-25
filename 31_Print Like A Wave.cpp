2fb21dd90e93433c0bf25a5c1ae983ca1e454332
S. Manna <90498811+codewar193@users.noreply.github.com>
2023-06-25 20:46:51 +0530

    easy #Coding Ninja

---
+++ 31_Print Like A Wave.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h> 
+vector<int> wavePrint(vector<vector<int>> arr, int nRows, int mCols)
+{
+    //Write your code here
+
+    vector<int> ans;
+
+    for(int col =0 ;col<mCols;col++)
+    {
+        // bottom to top
+        if(col&1)
+        {
+            for(int row = nRows-1;row>=0;row--)
+            {
+                ans.push_back(arr[row][col]);
+            }
+        }else{ //top to bottom
+           for(int row = 0;row<nRows;row++)
+            {
+                ans.push_back(arr[row][col]);
+            }
+        }
+    }
+    return ans;
+}
