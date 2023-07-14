f1725cbe21203afea408afba49676f7c3cc5e2a4
S. Manna <90498811+codewar193@users.noreply.github.com>
2023-07-14 23:33:02 +0530

    gfg #easy

--- q35_check-for-balanced-tree.cpp
+++ q35_check-for-balanced-tree.cpp
@@ -34,3 +34,38 @@ class Solution{
         }
     }
 };
+//method-2
+class Solution {
+public:
+    // Function to check whether a binary tree is balanced or not.
+    pair<bool, int> balanceFast(Node* root)
+    {
+        if (root == NULL)
+        {
+            pair<bool, int> p = make_pair(true, 0);
+            return p;
+        }
+
+        pair<bool, int> left = balanceFast(root->left);
+        pair<bool, int> right = balanceFast(root->right);
+
+        bool diff = abs(left.second - right.second) <= 1;
+        pair<bool, int> ans;
+        ans.second = max(left.second, right.second) + 1; // Update the height of the current node
+
+        if (left.first && right.first && diff)
+        {
+            ans.first = true;
+        }
+        else {
+            ans.first = false;
+        }
+        return ans;
+    }
+
+    bool isBalanced(Node* root)
+    {
+        // Your Code here
+        return balanceFast(root).first;
+    }
+};
